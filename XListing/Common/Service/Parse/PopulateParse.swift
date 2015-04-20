f3e95d3455070f1ec43e2d2d2ded4f71142f8b3f
Lance Zhu <lance.zhu@zenchatapp.com>
2015-04-20 07:41:10 -0700

    Clean up PopulateParse.

--- XListing/Common/Service/Parse/PopulateParse.swift
+++ XListing/Common/Service/Parse/PopulateParse.swift
@@ -9,7 +9,6 @@
 import Foundation
 //import SwiftyJSON
 import SwiftTask
-//import Dollar
 
 public class PopulateParse {
     
@@ -107,85 +106,6 @@ public class PopulateParse {
             }
     }
     
-    
-//    private typealias SaveTask = Task<Int, Bool, NSError>
-//    private let businessInteractor = BusinessInteractor(businessDataManager: BusinessDataManager(), geolocationDataManager: GeolocationDataManager())
-//
-//    public func populate() {
-//        populateFromJSON()
-//        
-//    }
-//    
-//    public func featuredizeByNameSChinese(name: String) {
-//        // create a task to find the business first
-//        let q = BusinessEntity.query()
-//        q.whereKey("nameSChinese", equalTo: name)
-//        let queryTask = businessInteractor.findBusinessBy(q)
-//        
-//        
-//        // create a task to save to the cloud
-//        let saveTask = queryTask
-//            .success { businessDomain -> SaveTask in
-//                if businessDomain.count > 1 {
-//                    fatalError("Found multiple records!")
-//                }
-//                
-//                // create a new BusinessDomain to update the business
-//                let updatedBusinessDomain = BusinessDomain()
-//                updatedBusinessDomain.objectId = businessDomain.first?.objectId
-//                updatedBusinessDomain.featured = true
-//                updatedBusinessDomain.timeStart = NSDate()
-//                updatedBusinessDomain.timeEnd = NSDate()
-//                
-//                return self.businessInteractor.saveBusiness(updatedBusinessDomain)
-//            }
-//        
-//        // process save tasks
-//        processAllSaveTasks([saveTask])
-//    }
-//    
-//    private func populateFromJSON() {
-//        var businessEntityArr = loadBusinessesFromJSON("localBizInfo", ofType: "json")
-//        
-//        // launch all tasks
-//        let uploadTasks = businessEntityArr.map { (business: BusinessDomain) -> SaveTask in
-//            
-//            return self.businessInteractor.saveBusiness(business)
-//        }
-//        
-//        processAllSaveTasks(uploadTasks)
-//        
-//    }
-//    
-//    ///
-//    /// Process every save task.
-//    ///
-//    private func processAllSaveTasks(tasks: [SaveTask]) {
-//        // ensure all upload tasks are successful
-//        Task
-//            .all(tasks)
-//            .progress { (oldProgress, newProgress) -> Void in
-//                println("Progress: \(newProgress.completedCount)/\(newProgress.totalCount)")
-//                return
-//            }
-//            .success { successes -> Void in
-//                if $.every(successes, callback: { $0 }) && successes.count == tasks.count {
-//                    println("Upload data to server completes successfully!")
-//                }
-//                else {
-//                    println("Upload fails!")
-//                }
-//            }
-//            .failure { (error, isCancelled) -> Void in
-//                if let error = error {
-//                    println("Error: \(error.localizedDescription)")
-//                }
-//                if isCancelled {
-//                    println("Tasks cancelled!")
-//                }
-//        }
-//    }
-//    
 //    ///
 //    /// Load data from JSON file
 //    ///
