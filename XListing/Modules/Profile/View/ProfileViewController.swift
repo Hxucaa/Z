ae25a5c0265242f910dbd08076d08f88f5e1e068
Bruce Li <bruce.li@zenchatapp.com>
2015-05-19 01:21:37 -0700

    set up storyboard for profile view

--- XListing/Modules/Profile/View/ProfileViewController.swift
+++ XListing/Modules/Profile/View/ProfileViewController.swift
@@ -10,6 +10,8 @@ public class ProfileViewController : UIViewController {
 
     public var profileVM: IProfileViewModel?
 
+    @IBOutlet weak var tableView: UITableView!
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
         println("I'm in ProfileViewController")
@@ -18,5 +20,90 @@ public class ProfileViewController : UIViewController {
     public override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+}
+
+
+/**
+*  UITableViewDataSource
+*/
+extension ProfileViewController : UITableViewDataSource {
+    /**
+    Asks the data source to return the number of sections in the table view.
+    
+    :param: tableView An object representing the table view requesting this information.
+    
+    :returns: The number of sections in tableView. The default value is 1.
+    */
+    public func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    /**
+    Tells the data source to return the number of rows in a given section of a table view. (required)
+    
+    :param: tableView The table-view object requesting this information.
+    :param: section   An index number identifying a section in tableView.
+    
+    :returns: The number of rows in section.
+    */
+    public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 3
+        
+    }
+    
+    /**
+    Asks the data source for a cell to insert in a particular location of the table view. (required)
+    
+    :param: tableView A table-view object requesting the cell.
+    :param: indexPath An index path locating a row in tableView.
+    
+    :returns: An object inheriting from UITableViewCell that the table view can use for the specified row. An assertion is raised if you return nil
+    */
+    public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var placeHolderCell = tableView.dequeueReusableCellWithIdentifier("ProfileCell", forIndexPath: indexPath) as! UITableViewCell
+        return placeHolderCell
+        
+    }
+    
+    
+    
+    public func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        
+        return 44
+        
+        
+    }
+    
+    public func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return ""
+    }
+    
+    public func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 2
+    }
+    
+    public func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 2
+    }
+}
+
+/**
+*  UITableViewDelegate
+*/
+extension ProfileViewController : UITableViewDelegate {
+    /**
+    Tells the delegate that the specified row is now selected.
+    
+    :param: tableView A table-view object informing the delegate about the new row selection.
+    :param: indexPath An index path locating the new selected row in tableView.
+    */
+    public func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        //tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+
+        
+        
     }
 }
