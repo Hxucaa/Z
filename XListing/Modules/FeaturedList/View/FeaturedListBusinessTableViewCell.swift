dc0906e320690cc673a465e43d0f55bbdd52d6d4
Lance Zhu <lance.zhu@zenchatapp.com>
2015-06-30 17:34:15 -0700

    XLIOS-29 Fix duplicate ETA label.

--- XListing/Modules/FeaturedList/View/FeaturedListBusinessTableViewCell.swift
+++ XListing/Modules/FeaturedList/View/FeaturedListBusinessTableViewCell.swift
@@ -17,6 +17,8 @@ public final class FeaturedListBusinessTableViewCell : UITableViewCell, Reactive
     @IBOutlet weak var businessNameLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var participationLabel: UILabel!
+    @IBOutlet weak var etaLabel: UILabel!
+    
     
     private var viewmodel: FeaturedBusinessViewModel!
     
@@ -34,18 +36,7 @@ public final class FeaturedListBusinessTableViewCell : UITableViewCell, Reactive
         businessNameLabel.rac_text <~ self.viewmodel.businessName
         cityLabel.rac_text <~ self.viewmodel.city
         participationLabel.rac_text <~ self.viewmodel.participation
-        
-        combineLatest(self.viewmodel.city.producer, self.viewmodel.eta.producer)
-            |> observeOn(UIScheduler())
-            |> start(next: { [unowned self] city, eta in
-                let cityNSString : NSString = city as NSString
-                let cityStrSize : CGSize = cityNSString.sizeWithAttributes([NSFontAttributeName: UIFont.systemFontOfSize(12.0)])
-                var etaLabel = UILabel(frame: CGRectMake(self.cityLabel.frame.origin.x + cityStrSize.width, self.cityLabel.frame.origin.y, 200, self.cityLabel.frame.height))
-                etaLabel.text = eta
-                etaLabel.font = etaLabel.font.fontWithSize(12.0)
-                etaLabel.textColor = UIColor.darkGrayColor()
-                self.addSubview(etaLabel)
-            })
+        etaLabel.rac_text <~ self.viewmodel.eta
         
         self.viewmodel.coverImageNSURL.producer
             |> start(next: { url in
