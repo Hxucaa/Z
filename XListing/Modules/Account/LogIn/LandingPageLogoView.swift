62fc7e4ddbb62b3b9d2fe2dea53240b5aa726d7c
Bruce Li <bruce.li@zenchatapp.com>
2015-06-30 20:49:50 -0700

    added logo

---
+++ XListing/Modules/Account/LogIn/LandingPageLogoView.swift
@@ -0,0 +1,18 @@
+//
+//  LandingPageLogoView.swift
+//  XListing
+//
+//  Created by Bruce Li on 2015-06-29.
+//  Copyright (c) 2015 ZenChat. All rights reserved.
+//
+
+import Foundation
+import XAssets
+
+@IBDesignable
+public final class LandingPageLogoView: UIView {
+    
+    public override func drawRect(rect: CGRect) {
+        AssetsKit.drawLandingIcon()
+    }
+}
