0c1a3d5697a3e25f7875d615c6f86296476e27fb
Lance Zhu <lance.zhu@zenchatapp.com>
2015-07-08 18:52:17 -0700

    XLIOS-42 Featured List View Controller now directly implements table view delegate and data source.

--- XListing/Modules/FeaturedList/View/FeaturedListViewController.swift
+++ XListing/Modules/FeaturedList/View/FeaturedListViewController.swift
@@ -23,7 +23,6 @@ public final class FeaturedListViewController: XUIViewController {
     
     // MARK: - Private variables
     private var viewmodel: IFeaturedListViewModel!
-    private var bindingHelper: ReactiveTableBindingHelper<FeaturedBusinessViewModel>!
     
     // MARK: - Setup Code
     public override func viewDidLoad() {
@@ -34,7 +33,6 @@ public final class FeaturedListViewController: XUIViewController {
         setupRefresh()
         setupNearbyButton()
         setupProfileButton()
-        setupTableView()
     }
 
     public override func didReceiveMemoryWarning() {
@@ -44,6 +42,7 @@ public final class FeaturedListViewController: XUIViewController {
     
     public override func viewDidAppear(animated: Bool) {
         viewmodel.presentAccountModule()
+        setupTableView()
     }
     
     public func bindToViewModel(viewmodel: IFeaturedListViewModel) {
@@ -51,14 +50,13 @@ public final class FeaturedListViewController: XUIViewController {
     }
     
     private func setupTableView() {
-        bindingHelper = ReactiveTableBindingHelper(
-            tableView: tableView,
-            sourceSignal: viewmodel.featuredBusinessViewModelArr.producer,
-            storyboardIdentifier: CellIdentifier
-            )
-            { [unowned self] pos in
-                self.viewmodel.pushDetailModule(pos)
-        }
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        self.viewmodel.featuredBusinessViewModelArr.producer
+            |> start(next: { [unowned self] _ in
+                self.tableView.reloadData()
+            })
     }
     
     private func setupRefresh() {
@@ -108,5 +106,43 @@ public final class FeaturedListViewController: XUIViewController {
         
         profileButton.target = pushProfile.unsafeCocoaAction
         profileButton.action = CocoaAction.selector
+    }
+}
+
+extension FeaturedListViewController : UITableViewDataSource, UITableViewDelegate {
+    /**
+    Tells the data source to return the number of rows in a given section of a table view. (required)
+    
+    :param: tableView The table-view object requesting this information.
+    :param: section   An index number identifying a section in tableView.
+    
+    :returns: The number of rows in section.
+    */
+    public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewmodel.featuredBusinessViewModelArr.value.count
+    }
+    
+    /**
+    Asks the data source for a cell to insert in a particular location of the table view. (required)
+    
+    :param: tableView A table-view object requesting the cell.
+    :param: indexPath An index path locating a row in tableView.
+    
+    :returns: An object inheriting from UITableViewCell that the table view can use for the specified row. An assertion is raised if you return nil
+    */
+    public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier(CellIdentifier) as! FeaturedListBusinessTableViewCell
+        cell.bindViewModel(viewmodel.featuredBusinessViewModelArr.value[indexPath.row])
+        return cell
+    }
+    
+    /**
+    Tells the delegate that the specified row is now selected.
+    
+    :param: tableView A table-view object informing the delegate about the new row selection.
+    :param: indexPath An index path locating the new selected row in tableView.
+    */
+    public func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        self.viewmodel.pushDetailModule(indexPath.row)
     }
 }
