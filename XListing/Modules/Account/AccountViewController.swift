cd1e50e65e2eb3a39596a90d6b5c0dbd447459cd
Lance Zhu <lance.zhu@zenchatapp.com>
2015-07-25 02:12:09 -0700

    XLIOS-105 Add animation between view transitions.

--- XListing/Modules/Account/AccountViewController.swift
+++ XListing/Modules/Account/AccountViewController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import ReactiveCocoa
-import Dollar
 
 private let EditInfoViewNibName = "EditInfoView"
 private let SignUpViewNibName = "SignUpView"
@@ -25,8 +24,14 @@ public final class AccountViewController: XUIViewController {
     private var editInfoView: EditInfoView!
     
     // MARK: Properties
+    
     private var viewmodel: IAccountViewModel!
-    private var disposables = [Disposable]()
+    /// A disposable that will dispose of any number of other disposables.
+    private let compositeDisposable = CompositeDisposable()
+    /**
+    A producer that handles transition of views. It also takes in a completion handler after transition is done.
+    */
+    private let (viewTransitionProducer, viewTransitionSink) = SignalProducer<(view: UIView, completion: (Bool -> Void)?), NoError>.buffer(1)
     
     // MARK: Setups
     public override func loadView() {
@@ -52,8 +57,12 @@ public final class AccountViewController: XUIViewController {
         setupLogIn()
         setupSignUp()
         setupEditInfo()
+        setupTransitions()
         
-        showLandingPageView()
+        // set initial view
+        view.addSubview(landingPageView)
+        addConstraintsToClipToAllSides(landingPageView)
+        
     }
     
     private func setupLandingPage() {
@@ -70,17 +79,23 @@ public final class AccountViewController: XUIViewController {
         
         let login = landingPageView.loginProxy
             |> start(next: { [weak self] in
-                self?.showLogInView()
+                if let this = self {
+                    // transition to log in view
+                    sendNext(this.viewTransitionSink, (view: this.logInView, completion: { success in this.logInView.startFirstResponder() }))
+                }
             })
         
         let signUp = landingPageView.signUpProxy
             |> start(next: { [weak self] in
-                self?.showSignUpView()
+                if let this = self {
+                    // transition to sign up view
+                    sendNext(this.viewTransitionSink, (view: this.signUpView, completion: { success in this.signUpView.startFirstResponder() }))
+                }
             })
         
-        disposables.append(skip)
-        disposables.append(login)
-        disposables.append(signUp)
+        compositeDisposable.addDisposable(skip)
+        compositeDisposable.addDisposable(login)
+        compositeDisposable.addDisposable(signUp)
     }
     
     private func setupLogIn() {
@@ -88,7 +103,10 @@ public final class AccountViewController: XUIViewController {
         
         let goBack = logInView.goBackProxy
             |> start(next: { [weak self] in
-                self?.showLandingPageView()
+                if let this = self {
+                    // transition to landing page view
+                    sendNext(this.viewTransitionSink, (view: this.landingPageView, completion: nil))
+                }
             })
         
         let finishLogIn = logInView.finishLoginProxy
@@ -103,8 +121,8 @@ public final class AccountViewController: XUIViewController {
                 self?.navigationController?.setNavigationBarHidden(false, animated: false)
             })
         
-        disposables.append(goBack)
-        disposables.append(finishLogIn)
+        compositeDisposable.addDisposable(goBack)
+        compositeDisposable.addDisposable(finishLogIn)
     }
     
     private func setupSignUp() {
@@ -112,16 +130,22 @@ public final class AccountViewController: XUIViewController {
         
         let goBack = signUpView.goBackProxy
             |> start(next: { [weak self] in
-                self?.showLandingPageView()
+                if let this = self {
+                    // transition to landing page view
+                    sendNext(this.viewTransitionSink, (view: this.landingPageView, completion: nil))
+                }
             })
 
         let finishSignUp = signUpView.finishSignUpProxy
             |> start(next: { [weak self] in
-                self?.showEditInfoView()
+                if let this = self {
+                    // transition to edit info view
+                    sendNext(this.viewTransitionSink, (view: this.editInfoView, completion: nil))
+                }
             })
         
-        disposables.append(goBack)
-        disposables.append(finishSignUp)
+        compositeDisposable.addDisposable(goBack)
+        compositeDisposable.addDisposable(finishSignUp)
     }
     
     private func setupEditInfo() {
@@ -129,11 +153,13 @@ public final class AccountViewController: XUIViewController {
         
         let presentUIImagePicker = editInfoView.presentUIImagePickerProxy
             |> start(next: { [weak self] imagePicker in
+                // present image picker
                 self?.presentViewController(imagePicker, animated: true, completion: nil)
             })
         
         let dismissUIImagePicker = editInfoView.dismissUIImagePickerProxy
             |> start(next: { [weak self] handler in
+                // dismiss image picker
                 self?.dismissViewControllerAnimated(true, completion: handler)
             })
         
@@ -150,14 +176,34 @@ public final class AccountViewController: XUIViewController {
                 self?.navigationController?.setNavigationBarHidden(false, animated: false)
             })
         
-        disposables.append(presentUIImagePicker)
-        disposables.append(dismissUIImagePicker)
-        disposables.append(finishEditInfo)
+        compositeDisposable.addDisposable(presentUIImagePicker)
+        compositeDisposable.addDisposable(dismissUIImagePicker)
+        compositeDisposable.addDisposable(finishEditInfo)
+    }
+    
+    private func setupTransitions() {
+        
+        // transition to next view.
+        let viewTransition = viewTransitionProducer
+            // forwards events along with the previous value. The first member is the previous value and the second is the current value.
+            |> combinePrevious((view: landingPageView, completion: nil))
+            |> start(next: { [unowned self] previous, current in
+                
+                // transition animation
+                self.animateTransition(previous.view, toView: current.view) { success in
+                    
+                    if let completion = current.completion {
+                        completion(success)
+                    }
+                }
+            })
+        
+        compositeDisposable.addDisposable(viewTransition)
     }
     
     deinit {
         // Dispose signals before deinit.
-        $.each(disposables) { $0.dispose() }
+        compositeDisposable.dispose()
         AccountLogVerbose("Account View Controller deinitializes.")
     }
     
@@ -169,21 +215,86 @@ public final class AccountViewController: XUIViewController {
     
     // MARK: Others
     
-    private func showLandingPageView() {
-        view = landingPageView
+    /**
+    Transition to a view with animation.
+    
+    :param: fromView   From a UIView.
+    :param: toView     To a UIView.
+    :param: completion Completion handler which takes in a parameter indicating success.
+    */
+    private func animateTransition<V: UIView>(fromView: V, toView: V, completion: (Bool -> Void)? = nil) {
+        UIView.transitionWithView(
+            view,
+            duration: 0.5,
+            options: UIViewAnimationOptions.TransitionCrossDissolve,
+            animations: { [unowned self] in
+                fromView.removeFromSuperview()
+                
+                self.view.addSubview(toView)
+                self.addConstraintsToClipToAllSides(toView)
+            },
+            completion: { [unowned self] finished in
+                
+                if let completion = completion {
+                    completion(finished)
+                }
+            }
+        )
     }
     
-    private func showSignUpView() {
-        view = signUpView
-        signUpView.startFirstResponder()
-    }
+    /**
+    Add constraints that clip to all sides of superview to subview.
     
-    private func showLogInView() {
-        view = logInView
-        logInView.startFirstResponder()
-    }
-    
-    private func showEditInfoView() {
-        view = editInfoView
+    :param: subview A UIView.
+    */
+    private func addConstraintsToClipToAllSides<V: UIView>(subview: V) {
+        // turn off autoresizing mask off to allow custom autolayout constraints
+        subview.setTranslatesAutoresizingMaskIntoConstraints(false)
+        
+        // add constraints
+        view.addConstraints(
+            [
+                // leading space to view is 0
+                NSLayoutConstraint(
+                    item: subview,
+                    attribute: NSLayoutAttribute.Leading,
+                    relatedBy: NSLayoutRelation.Equal,
+                    toItem: view,
+                    attribute: NSLayoutAttribute.Leading,
+                    multiplier: 1.0,
+                    constant: 0.0
+                ),
+                // top space to view is 0
+                NSLayoutConstraint(
+                    item: subview,
+                    attribute: NSLayoutAttribute.Top,
+                    relatedBy: NSLayoutRelation.Equal,
+                    toItem: view,
+                    attribute: NSLayoutAttribute.Top,
+                    multiplier: 1.0,
+                    constant: 0.0
+                ),
+                // trailing space to view is 0
+                NSLayoutConstraint(
+                    item: subview,
+                    attribute: NSLayoutAttribute.Trailing,
+                    relatedBy: NSLayoutRelation.Equal,
+                    toItem: view,
+                    attribute: NSLayoutAttribute.Trailing,
+                    multiplier: 1.0,
+                    constant: 0.0
+                ),
+                // botom space to view is 0
+                NSLayoutConstraint(
+                    item: subview,
+                    attribute: NSLayoutAttribute.Bottom,
+                    relatedBy: NSLayoutRelation.Equal,
+                    toItem: view,
+                    attribute: NSLayoutAttribute.Bottom,
+                    multiplier: 1.0,
+                    constant: 0.0
+                )
+            ]
+        )
     }
 }
