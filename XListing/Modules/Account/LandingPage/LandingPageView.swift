61bd508b40131e185332ebc8c6b8d966080b4ce7
Lance Zhu <lance.zhu@zenchatapp.com>
2015-08-02 01:47:50 -0700

    XLIOS-129 Add a back button to Landing Page.

--- XListing/Modules/Account/LandingPage/LandingPageView.swift
+++ XListing/Modules/Account/LandingPage/LandingPageView.swift
@@ -19,6 +19,7 @@ import ReactiveCocoa
 
 private let StartUpButtonsViewNibName = "StartUpButtonsView"
 private let RePromptButtonsViewNibName = "RePromptButtonsView"
+private let BackButtonNibName = "BackButton"
 
 @IBDesignable
 public final class LandingPageView : UIView {
@@ -28,6 +29,7 @@ public final class LandingPageView : UIView {
     @IBOutlet private weak var loginButton: UIButton!
     @IBOutlet private weak var signUpButton: UIButton!
     @IBOutlet private weak var dividerLabel: UILabel?
+    @IBOutlet private weak var backButton: UIButton?
     
     // MARK: - Proxies
     
@@ -50,11 +52,25 @@ public final class LandingPageView : UIView {
     private let (_signUpProxy, _signUpSink) = SimpleProxy.proxy()
     
     
-    // MARK: Properties
+    // MARK: - Properties
     private let viewmodel = MutableProperty<LandingPageViewModel?>(nil)
     private let compositeDisposable = CompositeDisposable()
     
-    // MARK: Setups
+    // MARK: - Actions
+    /// Skip this view
+    private lazy var skipAction: Action<UIButton, Void, NoError> = Action<UIButton, Void, NoError> { [weak self] button in
+        return SignalProducer { sink, disposable in
+            if let this = self {
+                // send event to skip proxy
+                sendNext(this._skipSink, ())
+            }
+            
+            // completes this action
+            sendCompleted(sink)
+        }
+    }
+    
+    // MARK: - Setups
     
     public override func awakeFromNib() {
         super.awakeFromNib()
@@ -62,17 +78,16 @@ public final class LandingPageView : UIView {
         compositeDisposable += viewmodel.producer
             |> ignoreNil
             |> start(next: { [weak self] viewmodel in
+                // conditionally load subviews
                 if viewmodel.rePrompt {
-                    let rePromptButtonsView = UINib(nibName: RePromptButtonsViewNibName, bundle: nil).instantiateWithOwner(self, options: nil).first as! UIView
-                    self?.addSubview(rePromptButtonsView)
-                    self?.addConstraintsToButtonsView(rePromptButtonsView)
+                    self?.setupButtonsView(RePromptButtonsViewNibName)
+                    self?.setupBackButton()
                 }
                 else {
-                    let startUpButtonsView = UINib(nibName: StartUpButtonsViewNibName, bundle: nil).instantiateWithOwner(self, options: nil).first as! UIView
-                    self?.addSubview(startUpButtonsView)
-                    self?.addConstraintsToButtonsView(startUpButtonsView)
+                    self?.setupButtonsView(StartUpButtonsViewNibName)
                 }
                 
+                // setup other UI elements
                 self?.setupLoginButton()
                 self?.setupSignUpButton()
                 self?.setupSkipButton()
@@ -80,6 +95,63 @@ public final class LandingPageView : UIView {
                 
             })
         
+    }
+    
+    private func setupButtonsView(nibName: String) {
+        
+        // load view
+        let view = UINib(nibName: nibName, bundle: nil).instantiateWithOwner(self, options: nil).first as! UIView
+        // add to subview
+        addSubview(view)
+        
+        // turn off autoresizing mask off to allow custom autolayout constraints
+        view.setTranslatesAutoresizingMaskIntoConstraints(false)
+        
+        // add constraints
+        addConstraints(
+            [
+                // set height to 109
+                NSLayoutConstraint(
+                    item: view,
+                    attribute: NSLayoutAttribute.Height,
+                    relatedBy: NSLayoutRelation.Equal,
+                    toItem: nil,
+                    attribute: NSLayoutAttribute.NotAnAttribute,
+                    multiplier: 1.0,
+                    constant: 109.0
+                ),
+                // set width to 247
+                NSLayoutConstraint(
+                    item: view,
+                    attribute: NSLayoutAttribute.Width,
+                    relatedBy: NSLayoutRelation.Equal,
+                    toItem: nil,
+                    attribute: NSLayoutAttribute.NotAnAttribute,
+                    multiplier: 1.0,
+                    constant: 247.0
+                ),
+                // center at X = 0
+                NSLayoutConstraint(
+                    item: view,
+                    attribute: NSLayoutAttribute.CenterX,
+                    relatedBy: NSLayoutRelation.Equal,
+                    toItem: self,
+                    attribute: NSLayoutAttribute.CenterX,
+                    multiplier: 1.0,
+                    constant: 0.0
+                ),
+                // botom space to view is 67
+                NSLayoutConstraint(
+                    item: self,
+                    attribute: NSLayoutAttribute.Bottom,
+                    relatedBy: NSLayoutRelation.Equal,
+                    toItem: view,
+                    attribute: NSLayoutAttribute.Bottom,
+                    multiplier: 1.0,
+                    constant: 67.0
+                )
+            ]
+        )
     }
     
     private func setupLoginButton() {
@@ -114,20 +186,7 @@ public final class LandingPageView : UIView {
         skipButton?.layer.masksToBounds = true
         skipButton?.layer.cornerRadius = 8
         
-        // Action to an UI event
-        let skip = Action<UIButton, Void, NoError> { [weak self] button in
-            return SignalProducer { sink, disposable in
-                if let this = self {
-                    // send event to skip proxy
-                    sendNext(this._skipSink, ())
-                }
-                
-                // completes this action
-                sendCompleted(sink)
-            }
-        }
-        
-        skipButton?.addTarget(skip.unsafeCocoaAction, action: CocoaAction.selector, forControlEvents: .TouchUpInside)
+        skipButton?.addTarget(skipAction.unsafeCocoaAction, action: CocoaAction.selector, forControlEvents: .TouchUpInside)
     }
     
     private func setupDividerLabel() {
@@ -136,6 +195,51 @@ public final class LandingPageView : UIView {
         dividerLabel?.layer.shadowOpacity = 0.5
         dividerLabel?.layer.shadowOffset = CGSize.zeroSize
         
+    }
+    
+    private func setupBackButton() {
+        
+        // load back button
+        let backButtonView = UINib(nibName: BackButtonNibName, bundle: nil).instantiateWithOwner(self, options: nil).first as! UIView
+        // add back button to subview
+        addSubview(backButtonView)
+        
+        // turn off autoresizing mask off to allow custom autolayout constraints
+        backButtonView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        
+        // leading margin 8.0
+        let backButtonLeading = NSLayoutConstraint(
+            item: backButtonView,
+            attribute: NSLayoutAttribute.Leading,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: self,
+            attribute: NSLayoutAttribute.LeadingMargin,
+            multiplier: 1.0,
+            constant: 8.0
+        )
+        backButtonLeading.identifier = "backButton leading constraint"
+        
+        // top margin 8.0
+        let backButtonTop = NSLayoutConstraint(
+            item: backButtonView,
+            attribute: NSLayoutAttribute.Top,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: self,
+            attribute: NSLayoutAttribute.TopMargin,
+            multiplier: 1.0,
+            constant: 8.0
+        )
+        backButtonTop.identifier = "backButton top constraint"
+        
+        // add constraints
+        addConstraints(
+            [
+                backButtonLeading,
+                backButtonTop
+            ]
+        )
+        
+        backButton?.addTarget(skipAction.unsafeCocoaAction, action: CocoaAction.selector, forControlEvents: .TouchUpInside)
     }
     
     deinit {
@@ -149,56 +253,4 @@ public final class LandingPageView : UIView {
     }
     
     // MARK: - Others
-    private func addConstraintsToButtonsView<V: UIView>(subview: V) {
-        
-        // turn off autoresizing mask off to allow custom autolayout constraints
-        subview.setTranslatesAutoresizingMaskIntoConstraints(false)
-        
-        // add constraints
-        self.addConstraints(
-            [
-                // set height to 109
-                NSLayoutConstraint(
-                    item: subview,
-                    attribute: NSLayoutAttribute.Height,
-                    relatedBy: NSLayoutRelation.Equal,
-                    toItem: nil,
-                    attribute: NSLayoutAttribute.NotAnAttribute,
-                    multiplier: 1.0,
-                    constant: 109.0
-                ),
-                // set width to 247
-                NSLayoutConstraint(
-                    item: subview,
-                    attribute: NSLayoutAttribute.Width,
-                    relatedBy: NSLayoutRelation.Equal,
-                    toItem: nil,
-                    attribute: NSLayoutAttribute.NotAnAttribute,
-                    multiplier: 1.0,
-                    constant: 247.0
-                ),
-                // center at X = 0
-                NSLayoutConstraint(
-                    item: subview,
-                    attribute: NSLayoutAttribute.CenterX,
-                    relatedBy: NSLayoutRelation.Equal,
-                    toItem: self,
-                    attribute: NSLayoutAttribute.CenterX,
-                    multiplier: 1.0,
-                    constant: 0.0
-                ),
-                // botom space to view is 67
-                NSLayoutConstraint(
-                    item: self,
-                    attribute: NSLayoutAttribute.Bottom,
-                    relatedBy: NSLayoutRelation.Equal,
-                    toItem: subview,
-                    attribute: NSLayoutAttribute.Bottom,
-                    multiplier: 1.0,
-                    constant: 67.0
-                )
-            ]
-        )
-    }
-
 }
