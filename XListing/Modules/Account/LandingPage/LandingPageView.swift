279b65bc423203016a12de75e624ba2eccac2771
Connor Wang <connor.wang@zenchatapp.com>
2015-08-02 22:13:19 -0700

    Fixed 'X' button positioning on landing / sign up / login pages

--- XListing/Modules/Account/LandingPage/LandingPageView.swift
+++ XListing/Modules/Account/LandingPage/LandingPageView.swift
@@ -207,7 +207,7 @@ public final class LandingPageView : UIView {
         // turn off autoresizing mask off to allow custom autolayout constraints
         backButtonView.setTranslatesAutoresizingMaskIntoConstraints(false)
         
-        // Trailing margin 8.0
+        // Leading margin 8.0
         let backButtonLeading = NSLayoutConstraint(
             item: backButtonView,
             attribute: NSLayoutAttribute.Leading,
