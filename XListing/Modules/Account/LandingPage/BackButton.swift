0a1486c59501d32d85392bf2c0b549842d90fe5d
anson.yao <anson.yao@zenchatapp.com>
2015-08-10 22:00:36 -0700

    workedonwrongbranch

---
+++ XListing/Modules/Account/LandingPage/BackButton.swift
@@ -0,0 +1,21 @@
+//
+//  BackButton.swift
+//  XListing
+//
+//  Created by Anson on 2015-08-09.
+//  Copyright (c) 2015 ZenChat. All rights reserved.
+//
+
+import UIKit
+
+class BackButton: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
