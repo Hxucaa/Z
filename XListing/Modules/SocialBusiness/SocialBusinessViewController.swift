83d7f7fac91bfe6aa07e724ae077bf1fcd85897e
anson.yao <anson.yao@zenchatapp.com>
2015-09-03 22:35:15 -0700

    setup of the first page

--- XListing/Modules/SocialBusiness/SocialBusinessViewController.swift
+++ XListing/Modules/SocialBusiness/SocialBusinessViewController.swift
@@ -12,17 +12,20 @@ import ReactiveCocoa
 import ReactiveArray
 import Dollar
 
-
-
 public final class SocialBusinessViewController : UIViewController {
     @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet weak var infoButton: UIButton!
+    @IBOutlet weak var startEventButton: UIButton!
     
     // MARK: - Properties
     private var viewmodel: ISocialBusinessViewModel!
     private let userCellIdentifier = "usercell"
     private let businessCellIdentifier = "businesscell"
-    
-    
+    private let businessHeightRatio = 0.6
+    private let userHeightRatio = 0.25
+    private let WTGBarHeight = CGFloat(70)
+    let screenWidth = UIScreen.mainScreen().bounds.size.width
+
     // MARK: - Bindings
     
     public func bindToViewModel(viewmodel: ISocialBusinessViewModel) {
@@ -37,6 +40,8 @@ public final class SocialBusinessViewController : UIViewController {
         let businessNib = UINib(nibName: "BusinessSocialBusinessCell", bundle: nil)
         tableView.registerNib(userNib, forCellReuseIdentifier: userCellIdentifier)
         tableView.registerNib(businessNib, forCellReuseIdentifier: businessCellIdentifier)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     override public func viewDidAppear(animated: Bool) {
@@ -52,7 +57,7 @@ extension SocialBusinessViewController: UITableViewDelegate, UITableViewDataSour
     public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         switch(section){
         case 0: return 1
-        case 1: return 1
+        case 1: return 10
         default: return 0
         }
     }
@@ -60,11 +65,31 @@ extension SocialBusinessViewController: UITableViewDelegate, UITableViewDataSour
     public func tableView(tableView: UITableView,
         cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
             switch(indexPath.section){
-            case 0: var cell = tableView.dequeueReusableCellWithIdentifier(userCellIdentifier) as! UserCell
+            case 0: var cell = tableView.dequeueReusableCellWithIdentifier(businessCellIdentifier) as! BusinessCell
+                cell.frame = CGRectMake(0, 0, CGFloat(screenWidth), CGFloat(screenWidth) * CGFloat(businessHeightRatio))
                 return cell
-            default: var cell = tableView.dequeueReusableCellWithIdentifier(businessCellIdentifier) as! BusinessCell
+            default: var cell = tableView.dequeueReusableCellWithIdentifier(userCellIdentifier) as!
+                UserCell
+                cell.frame = CGRectMake(0, 0, CGFloat(screenWidth), CGFloat(screenWidth) * CGFloat(userHeightRatio))
                 return cell
             }
+    }
+    
+    public func tableView(tableView: UITableView, heightForRowAtIndexPath
+        indexPath: NSIndexPath) -> CGFloat{
+        switch(indexPath.section){
+        case 0: return CGFloat(screenWidth) * CGFloat(businessHeightRatio)
+        default: return CGFloat(screenWidth) * CGFloat(userHeightRatio)
+        }
+    }
+    
+    public func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView?{
+        var view: UIView?
+        if section == 0{
+            view = NSBundle.mainBundle().loadNibNamed("WTGBar", owner: self, options: nil)[0] as? UIView
+            view?.frame = CGRectMake(0, 0, CGFloat(screenWidth), WTGBarHeight)
+        }
+        return view
     }
         
 }
