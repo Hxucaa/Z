fb184266432e2656e3daf86a7ad9f733d158e39f
Lance Zhu <lance.zhu@zenchatapp.com>
2015-09-09 16:15:08 -0700

    XLIOS-192 Add constraints to table view.

--- XListing/Modules/Business Detail & Social/BusinessDetail/BusinessDetailViewController.swift
+++ XListing/Modules/Business Detail & Social/BusinessDetail/BusinessDetailViewController.swift
@@ -18,6 +18,8 @@ public final class BusinessDetailViewController : XUIViewController {
     // MARK: - UI Controls
     private lazy var tableView: UITableView = {
         let tableView = UITableView(frame: CGRectMake(0, 0, 600, 600), style: UITableViewStyle.Grouped)
+        tableView.showsHorizontalScrollIndicator = false
+        tableView.opaque = true
         
         return tableView
     }()
@@ -30,8 +32,37 @@ public final class BusinessDetailViewController : XUIViewController {
     public override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.addSubview(tableView)
+        
+        constrain(tableView) { view in
+            view.top == view.superview!.top
+            view.trailing == view.superview!.trailing
+            view.bottom == view.superview!.bottom
+            view.leading == view.superview!.leading
+        }
+        
         view.opaque = true
         view.backgroundColor = UIColor.whiteColor()
+    }
+    
+    public override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
+        
+        /**
+        Assigning UITableView delegate has to happen after signals are established.
+        
+        - tableView.delegate is assigned to self somewhere in UITableViewController designated initializer
+        
+        - UITableView caches presence of optional delegate methods to avoid -respondsToSelector: calls
+        
+        - You use -rac_signalForSelector:fromProtocol: and RAC creates method implementation for you in runtime. But UITableView knows nothing about this implementation, it still thinks that there's no such method
+        
+        The solution is to reassign delegate after all your -rac_signalForSelector:fromProtocol: calls:
+        */
+//        tableView.delegate = nil
+//        tableView.delegate = self
     }
     
     // MARK: - Bindings
@@ -42,3 +73,7 @@ public final class BusinessDetailViewController : XUIViewController {
     
     // MARK: - Others
 }
+
+//extension BusinessDetailViewController : UITableViewDelegate, UITableViewDataSource  {
+//
+//}
