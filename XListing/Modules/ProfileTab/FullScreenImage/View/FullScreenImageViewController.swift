342bc2b6503fb80e316dd90dfecfac8a6955e42a
Bruce Li <bruce.li@zenchatapp.com>
2015-10-14 20:24:07 -0700

    built UI for full screen image viewer

--- XListing/Modules/ProfileTab/FullScreenImage/View/FullScreenImageViewController.swift
+++ XListing/Modules/ProfileTab/FullScreenImage/View/FullScreenImageViewController.swift
@@ -7,31 +7,58 @@
 //
 
 import UIKit
+import Cartography
+import ReactiveCocoa
 
 public final class FullScreenImageViewController: UIViewController {
 
     
     private var viewmodel : IFullScreenImageViewModel!
     
+    private lazy var photo : UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "profilepicture"))
+        return imageView
+    }()
+    
+    private lazy var backButton: UIButton = {
+        let button = UIButton()
+        let attributes = [NSForegroundColorAttributeName: UIColor.whiteColor(), NSFontAttributeName: UIFont(name: "FontAwesome", size: 17)!]
+        
+        var attributedString = NSAttributedString(string: Icons.Chevron + " 返回", attributes: attributes)
+        button.setAttributedTitle(attributedString, forState: UIControlState.Normal)
+        
+        return button
+        }()
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = UIColor.blackColor()
+        view.addSubview(photo)
+        view.addSubview(backButton)
+        let goBack = Action<UIButton, Void, NoError> { button in
+            return SignalProducer { sink ,disposable in
+                self.dismissViewControllerAnimated(true, completion: nil)
+                sendCompleted(sink)
+            }
+        }
+        
+        backButton.addTarget(goBack.unsafeCocoaAction, action: CocoaAction.selector, forControlEvents: UIControlEvents.TouchUpInside)
+        
+        constrain(photo) { view in
+            view.leading == view.superview!.leading
+            view.trailing == view.superview!.trailing
+        }
+        
+        constrain(backButton) { view in
+            view.leading == view.superview!.leadingMargin
+            view.height == 64
+            
+        }
         // Do any additional setup after loading the view.
     }
 
     public func bindToViewModel(viewmodel: IFullScreenImageViewModel) {
         self.viewmodel = viewmodel
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
