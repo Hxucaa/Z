ab228ddb7de4f9c517e2cb6286b373c71564506b
Bruce Li <bruce.li@zenchatapp.com>
2015-10-21 00:23:20 -0700

    XLIOS-307 #time 0h #comment add files

---
+++ XListing/Modules/FullScreenImage/FullScreenImageViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  XListing
+//
+//  Created by Bruce Li on 2015-10-14.
+//  Copyright (c) 2015 ZenChat. All rights reserved.
+//
+
+import Foundation
+
+public final class FullScreenImageViewModel : IFullScreenImageViewModel {
+
+    public init(imageService: IImageService) {
+
+    }
+}
